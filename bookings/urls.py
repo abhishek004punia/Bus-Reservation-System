6765d71ab14e4bcbfd516a2a6f10e55a946edb56
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-16 18:46:02 +0530

    Add PDF  ticket generation using ReportLab

--- bookings/urls.py
+++ bookings/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("create/<int:schedule_id>/", views.create_booking, name="create_booking"),
     path("search/", views.search_bus, name="search_bus"),
     path("detail/<int:pk>/", views.booking_detail, name="booking_detail"),
+    path("ticket/<int:pk>/pdf/", views.download_ticket, name="download_ticket"),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 from .models import Booking
 from .forms import BookingForm, SearchForm
 from schedules.models import Schedule
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
 
 
 @login_required
@@ -122,3 +124,71 @@ def booking_detail(request, pk):
             "booking": booking,
         },
     )
+
+
+
+@login_required
+def download_ticket(request, pk):
+    booking = get_object_or_404(
+        Booking,
+        pk=pk,
+        user=request.user
+    )
+
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = (
+        f'attachment; filename="ticket_{booking.id}.pdf"'
+    )
+
+    pdf = canvas.Canvas(response)
+
+    pdf.setTitle("Bus Reservation Ticket")
+
+    pdf.setFont("Helvetica-Bold", 18)
+    pdf.drawString(170, 800, "BUS RESERVATION TICKET")
+
+    pdf.setFont("Helvetica", 12)
+
+    y = 760
+
+    pdf.drawString(50, y, f"Booking ID : BRS{booking.id:06d}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Passenger : {booking.passenger_name}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Age : {booking.passenger_age}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Gender : {booking.passenger_gender}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Mobile : {booking.mobile_number}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Bus : {booking.schedule.bus}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Route : {booking.schedule.route}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Travel Date : {booking.schedule.travel_date}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Departure : {booking.schedule.departure_time}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Arrival : {booking.schedule.arrival_time}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Seat Number : {booking.seat_number}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Fare : ₹ {booking.schedule.fare}")
+    y -= 25
+
+    pdf.drawString(50, y, f"Status : {booking.status}")
+
+    pdf.save()
+
+    return response
