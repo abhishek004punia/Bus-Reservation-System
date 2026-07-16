9b74d9ffad8c2ae3f273a7389dc2a827d2bdc083
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-16 15:37:37 +0530

    Add user registration functionlity

---
+++ accounts/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from users.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+        ]
+        
