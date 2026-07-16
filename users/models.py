7f69784332eb6b3ce9f6dba821abab21ef2999dd
Abhishek Punia <abhishek004punia@gmail.com>
2026-07-16 15:03:48 +0530

    Implement custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ADMIN = "ADMIN"
+    OPERATOR = "OPERATOR"
+    CUSTOMER = "CUSTOMER"
+
+    ROLE_CHOICES = [
+        (ADMIN, "Admin"),
+        (OPERATOR, "Operator"),
+        (CUSTOMER, "Customer"),
+    ]
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default=CUSTOMER,
+    )
+
+    def __str__(self):
+        return self.username
